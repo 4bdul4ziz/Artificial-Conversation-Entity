9b73a7ca19505b4f01d309a310f542f95551ef41
Subhranshu Pati <subhranshupati0412@gmail.com>
2022-01-11 23:54:17 +0530

    Resposive Navbar

--- website/src/components/Navbar/Navbar.jsx
+++ website/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,72 @@
-import React from 'react'
-import './Navbar.scss'
+import {React, useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/logo.svg'
+import './Navbar.scss';
 
 const Navbar = () => {
-    return (
-        <div>
-            Navbar
-        </div>
-    )
-}
+    const [toggleMenu, setToggleMenu] = useState(false);
 
-export default Navbar
+    const Menu=()=>(
+        <>
+            <p><a href="#home">Home</a></p>
+            <p><a href="#wgpt3">What is GPT3?</a></p>
+            <p><a href="#possibility">Open AI</a></p>
+            <p><a href="#features">Case Studies</a></p>
+            <p><a href="#blog">Library</a></p>
+        </>
+    )
+    
+    const Sign=()=>(
+        <>
+            <p>Sign in</p>
+            <button type="button">Sign up</button>
+        </>
+    )
+
+    return (
+        <div className="gpt3__navbar">
+            <div className="gpt3__navbar-links">
+                <div className="gpt3__navbar-links_logo">
+                    <img src={logo} alt='HADES Logo'/>
+                </div>
+                <div className="gpt3__navbar-links_container">
+                    <Menu/>
+                </div>
+            </div>
+            <div className="gpt3__navbar-sign">
+                <Sign/>
+            </div>
+            <div className="gpt3__navbar-menu">
+                {
+                    toggleMenu
+                    ? 
+                    <RiCloseLine 
+                    color="#fff" 
+                    size={27} 
+                    onClick={() => setToggleMenu(false)} 
+                    />
+                    : 
+                    <RiMenu3Line 
+                    color="#fff" 
+                    size={27} 
+                    onClick={() => setToggleMenu(true)} 
+                    />
+                }
+                {
+                    toggleMenu && (
+                        <div className="gpt3__navbar-menu_container scale-up-center">
+                            <div className="gpt3__navbar-menu_container-links">
+                                <Menu/>
+                            </div>
+                            <div className="gpt3__navbar-menu_container-links-sign">
+                                <Sign/>
+                            </div>
+                        </div>
+                    )
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
