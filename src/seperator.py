a754e865023fee1fde52de6f0c1eba67eec54547
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2022-01-12 23:46:44 +0530

    train_loader config update

--- src/seperator.py
+++ src/seperator.py
@@ -5,9 +5,12 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
+from mother import NeuralNet
 
-
-with open('src/data.json', 'r') as f:
+"""
+forgive me for not commenting stuff :sadkek: I'll document this properly once i generate some more braincells.
+"""
+with open('data.json', 'r') as f:
     intents = json.load(f)
 
 all_words = []
@@ -27,7 +30,8 @@ ignore_words = ['?', '!', '.']
 all_words = [stem(w) for w in all_words if w not in ignore_words]
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
-print(tags)
+
+
 
 x_train = []
 y_train = []
@@ -41,7 +45,32 @@ for (pattern_sentence, tag) in xy:
 x_train = np.array(x_train)
 y_train = np.array(y_train)
 
+"""
+Hyperparameters, uhh yeah- does some magic shit, configure this to change the train loader configs.
+"""
+batch_size = 8
+hidden_size = 8
+output_size = len(tags)
+input_size = len(x_train[0])
+print(input_size, len(all_words))
+print(output_size, tags)
+
+
 class ChatDataset(Dataset):
     def __init__(self):
-        pass 
-    #will do later
+        self.n_samples = len(all_words)
+        self.x_data = x_train
+        self.y_data = y_train
+
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+    def __len__(self):
+        return self.n_samples 
+
+dataset = ChatDataset()
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+        
+model = NeuralNet(input_size, hidden_size, output_size)
+
+ 
