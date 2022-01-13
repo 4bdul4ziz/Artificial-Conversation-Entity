f48b97b9c9a2de7e601cadeed7313331e040ff98
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2022-01-13 15:00:53 +0530

    basic interfacing for protypes

---
+++ src/interface.py
@@ -0,0 +1,52 @@
+import random
+import json
+from nltk import probability
+import torch
+from mother import NeuralNet
+from nltk_functions import bag_of_words, tokenize
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('data.json', 'r') as f:
+    responses = json.load(f)
+
+file = "data.pth"
+data = torch.load(file)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data["all_words"]
+tags = data["tags"]
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+bot_name = "Hades"
+print("Hades wants to spank you, to escape, type 'quit'.")
+while True:
+    sentence = input('\nYou: ')
+    if sentence == 'quit':
+        break
+
+    sentence = tokenize(sentence)
+    x = bag_of_words(sentence, all_words)
+    x = x.reshape(1, x.shape[0])
+    x = torch.from_numpy(x)
+
+    output = model(x)
+    _, predicted = torch.max(output, dim=1)
+    tag = tags[predicted.item()]
+
+    probability = torch.softmax(output, dim=1)
+    probability = probability[0][predicted.item()]
+
+    if probability.item() > 0.75:
+        for intent in responses["intents"]:
+            if tag == intent["tag"]:
+                print(f"\n{bot_name}: {random.choice(intent['responses'])}")
+    else:
+        print(f"{bot_name}: Tf you mean?")
