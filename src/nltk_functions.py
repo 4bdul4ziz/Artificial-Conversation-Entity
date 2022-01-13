e1d649513f408970328accf58fcbb0fe3877cd33
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2022-01-13 13:57:02 +0530

    nltk updates

--- src/nltk_functions.py
+++ src/nltk_functions.py
@@ -13,11 +13,11 @@ def stem(word):
     return stemmer.stem(word.lower())
 
 
-def bag_of_words(tokenized_sentence, words):
+def bag_of_words(tokenized_sentence, all_words):
     sentence_words = [stem(word) for word in tokenized_sentence]
 
-    bag = np.zeros(len(words), dtype = np.float64)
-    for i, w in enumerate(words):
+    bag = np.zeros(len(all_words), dtype = np.float32)
+    for i, w in enumerate(all_words):
         if w in sentence_words:
             bag[i] = 1
     return bag
