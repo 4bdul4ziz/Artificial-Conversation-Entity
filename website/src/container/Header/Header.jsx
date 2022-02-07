ee853804a39ddaff5f81460a0f002b9c1874ea9e
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2022-02-07 12:36:32 +0530

    header render updates

--- website/src/container/Header/Header.jsx
+++ website/src/container/Header/Header.jsx
@@ -6,14 +6,10 @@ import ai from '../../assets/ai.png'
 const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
-      <h1 className="gradient__text">Let&apos;s Build Something amazing with Tensor Flow</h1>
-      <p>And let that real shit provoke
-        See, you's a wannabe 'loc, and you'll get smoked, and I hope
-        That your fans understand when you talk about sprayin' me
-        The same records that you makin' is payin' me
+      <h1 className="gradient__text">An Artificial Coversing Entity</h1>
+      <p>With the help of neural networks, we can create a conversational entity that can answer questions about the world.
       </p><p>
-        Motherfuck Dre! Motherfuck Snoop! Motherfuck Death Row!
-        Yo, and here comes my left blow.
+        By - Abdul Aziz, Abhinav Bhatia & Subhranshu Pati.
       </p>
 
       <div className="gpt3__header-content__input">
