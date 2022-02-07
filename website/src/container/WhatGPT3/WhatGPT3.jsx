2f5150d3546879c09ae8a3ada0e746f8fd46c09d
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2022-02-07 12:36:09 +0530

    topic edits

--- website/src/container/WhatGPT3/WhatGPT3.jsx
+++ website/src/container/WhatGPT3/WhatGPT3.jsx
@@ -4,53 +4,43 @@ import './WhatGPT3.scss'
 
 const WhatGPT3 = () => {
     return (
-        <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
-            <div className="gpt3__whatgpt3-feature">
-            <Feature title="What is HADES" 
-            text="Well I'm peepin' and I'm creepin' and I'm creepin'
-            But I damn near got capped 'cause my beeper kept beepin'
-            Now it's time for me to make my impression felt
-            So sit back, relax, and strap on your seatbelt
-            You never been on a ride like this before
-            With a producer who can rap and control the maestro
-            At the same time with the dope rhyme that I kick
-            You know and I know, I flow some ol' funky shit
-            To add to my collection, the selection
-            Symbolizes dope, take a toke, but don't choke
-            If you do, you'll have no clue
-            Of what me and my homie Snoop Dogg came to do
-            It's like this and like that and like this and uh
-            It's like that and like this and like that and uh
-            It's like this
-            And who gives a fuck about those?
-            So just chill 'til the next episode"
-            />
-            </div>
-            <div className="gpt3__whatgpt3-heading">
-            <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
-            </div>
-            <div className="gpt3__whatgpt3-container">
-            <Feature title="Python" 
-            text="It's like this and like that and like this and uh
-            It's like that and like this and like that and uh
-            It's like this and like that and like this and uh
-            Dre, creep to the mic like a phantom" 
-            />
-            <Feature title="TensorFlow" 
-            text="One, two, three and to the four
-            Snoop Doggy Dogg and Dr. Dre is at the door
-            Ready to make an entrance, so back on up
-            ('Cause you know we're 'bout to rip shit up)" 
-            />
-            <Feature title="NLP" 
-            text="Gimme the microphone first, so I can bust like a bubble
-            Compton and Long Beach together, now you know you in trouble
-            Ain't nothin' but a G thang, baby
-            Two loc'ed out niggas so we're crazy" 
-            />
-            </div>
-      </div>
+        <>
+            <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+                <div className="gpt3__whatgpt3-feature">
+                    <Feature title="What is HADES?" 
+                    text="HADES is a conversational entity that can answer questions about the world.
+                    It is built using a neural network that is trained on a large corpus of questions and answers.
+                    Having a conversational entity is a great way to get answers to your questions.
+                    Things like weather, news, and more can be answered by the conversational entity.
+                    Customer Support can also be answered by the conversational entity.
+                    This is a great way to automate your customer support.
+                    Improvise the accuracy by adding in more datasets and training the neural network."
+                    />
+                </div>
+                <div className="gpt3__whatgpt3-heading"> 
+                    <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+                </div>
+                <div className="gpt3__whatgpt3-container">
+                    <Feature title="Python" 
+                    text="The Python language is a general-purpose, high-level programming language.
+                    It has efficient high-level data structures and a simple but effective approach to object-oriented programming.
+                    It's an easy language to learn and use. It is useful in ML, AI, and many other fields." 
+                    />
+                    <Feature title="Pytorch" 
+                    text="Pytorch is a machine learning library for Python. It is a deep learning framework that is used for deep learning. 
+                    It is a high-level API for training neural networks. It is used for deep learning models such as image recognition, text classification, and speech recognition.
+                    Our project is built using Pytorch. The training of the neural network is done using the Pytorch library."
+
+                    />
+                    <Feature title="NLP" 
+                    text="Natural Language Processing (NLP) is a field of computer science that deals with the interactions between computers and human (natural) languages.
+                    NLP is used to analyze text, speech, and audio data. Our project is built using NLP. It makes use of the NLTK library. 
+                    It is a library of Python modules that implement natural language processing algorithms. The usecase of NLP is to analyze text, speech, and audio data." 
+                    />
+                </div>
+        </div>
+    </>
     )
 }
-
+//EZ
 export default WhatGPT3
